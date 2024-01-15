bfa349004912691db06ed25b9f85dc6d369af1a3
Mrowr Purr <mrowr.purr@gmail.com>
2024-01-15 08:44:45 -0800

    README and TODOs

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,9 @@
 import * as vscode from "vscode";
 import { RegisterSpecsTestExtension } from "./TestExplorer";
 
+// TODO: GROUPS!
+// TODO: TAGS!
+
 export function activate(context: vscode.ExtensionContext) {
     RegisterSpecsTestExtension(context);
 }
